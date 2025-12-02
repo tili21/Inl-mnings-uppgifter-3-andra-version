62c9786e2e69a71a395b4c949ba55666cd95c486
tili21 <tili08001@utb.vaxjo.se>
2025-12-02 23:40:37 +0100

    Add files via upload

---
+++ Program (4).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är låten (minuter)?");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många sekunder?");
+            int sek = int.Parse(Console.ReadLine());
+
+            int totalSek = min * 60 + sek;
+
+            int minTillåtet = 2 * 60 + 45; // 165 sek
+            int maxTillåtet = 4 * 60 + 20; // 260 sek
+
+            if (totalSek >= minTillåtet && totalSek <= maxTillåtet)
+            {
+                Console.WriteLine("Låten får spelas på radiostationen.");
+            }
+            else
+            {
+                Console.WriteLine("Låten får inte spelas.");
+            }
+        }
+    }
+}
